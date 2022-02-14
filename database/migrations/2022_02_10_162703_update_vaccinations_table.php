05be1d4ae4aa8792b6691f1cc06b80edce9387c0
Chisanga Louis Siwale <chisanga.siwale@gmail.com>
2022-02-14 14:21:57 +0200

    Nationality issue on certificate

---
+++ database/migrations/2022_02_10_162703_update_vaccinations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateVaccinationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vaccinations', function (Blueprint $table) {
+            $table->timestamp('source_updated_at', 0)->nullable()->after('record_id');
+            $table->timestamp('source_created_at', 0)->nullable()->after('record_id');
+            $table->string('event_uid')->after('record_id');
+            $table->renameColumn('event_uid', 'source_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vaccinations', function (Blueprint $table) {
+            $table->renameColumn('source_id', 'event_uid');
+            $table->dropColumn('source_created_at');
+            $table->dropColumn('source_updated_at');
+        });
+    }
+}
