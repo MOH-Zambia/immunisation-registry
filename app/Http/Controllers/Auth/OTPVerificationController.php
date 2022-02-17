9fe522a3bcc93a45e88c8fcaa0213e26f18fcc83
Chisanga Louis Siwale <chisanga.siwale@gmail.com>
2022-02-17 09:25:49 +0200

    OTPVerificationController

--- app/Http/Controllers/Auth/OTPVerificationController.php
+++ app/Http/Controllers/Auth/OTPVerificationController.php
@@ -46,9 +46,9 @@ class OTPVerificationController extends AppBaseController
 
         curl_setopt_array($ch, array(
             CURLOPT_URL => $url,
-            CURLOPT_HEADER => TRUE,
+//            CURLOPT_HEADER => TRUE,
             CURLOPT_RETURNTRANSFER => true,
-            CURLINFO_HEADER_OUT => TRUE
+//            CURLINFO_HEADER_OUT => TRUE
         ));
 
         //Ignore SSL certificate verification
